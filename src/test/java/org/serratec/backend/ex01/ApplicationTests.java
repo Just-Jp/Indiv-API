98e5f3fe05c21719d0045deb527242c63c6b891c
João Pedro B. Dias <jotape_dias@outlook.com>
2025-05-20 09:36:27 -0300

    adiciona estrutura inicial da API

---
+++ src/test/java/org/serratec/backend/ex01/ApplicationTests.java
@@ -0,0 +1,13 @@
+package org.serratec.backend.ex01;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
