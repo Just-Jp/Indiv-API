5f494e7eded8259a005f869715bfc53bad0b2910
João Pedro B. Dias <jotape_dias@outlook.com>
2025-05-20 10:19:08 -0300

    adiciona relação entre tabelas

--- src/main/java/org/serratec/backend/models/Funcionario.java
+++ src/main/java/org/serratec/backend/models/Funcionario.java
@@ -1,11 +1,18 @@
 package org.serratec.backend.models;
 
+import java.util.ArrayList;
+import java.util.List;
+
+import jakarta.persistence.CascadeType;
 import jakarta.persistence.Column;
 import jakarta.persistence.Entity;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
+import jakarta.persistence.JoinColumn;
+import jakarta.persistence.OneToMany;
 import jakarta.persistence.Table;
+import jakarta.validation.Valid;
 import jakarta.validation.constraints.NotBlank;
 import jakarta.validation.constraints.Size;
 
@@ -21,10 +28,16 @@ public class Funcionario {
 	@Size(max = 100)
 	@Column(nullable = false, length = 100)
 	private String nome;
+	
+	@OneToMany(cascade = CascadeType.ALL, orphanRemoval = true)
+	@JoinColumn(name = "funcionario_id")
+    @Valid
+	private List<Endereco> enderecos = new ArrayList<>();
 
-	public Funcionario(Long id, String nome) {
+	public Funcionario(Long id, String nome, List<Endereco> enderecos) {
 		this.id = id;
 		this.nome = nome;
+		this.enderecos = enderecos;
 	}
 	
 	public Funcionario() {
@@ -45,4 +58,12 @@ public class Funcionario {
 	public void setNome(String nome) {
 		this.nome = nome;
 	}
+
+	public List<Endereco> getEnderecos() {
+		return enderecos;
+	}
+
+	public void setEnderecos(List<Endereco> enderecos) {
+		this.enderecos = enderecos;
+	}	
 }
